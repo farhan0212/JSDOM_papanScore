2c2bc59bec6053fc8620d7bab10c1f00162205bf
farhan0212 <farhan.ramadan0212@gmail.com>
2024-09-08 20:37:23 +0700

    menambahkan fitur change

--- app.js
+++ app.js
@@ -3,31 +3,44 @@ const tambahP2 = document.querySelector("#player-2");
 const reset = document.querySelector("#reset");
 const skorP1 = document.querySelector("#p1-display");
 const skorP2 = document.querySelector("#p2-display");
+const winPoint = document.querySelector("#winPoint");
 
-let winGame = 5;
 let isWin = false;
+let winGame = parseInt(winPoint.value);
 
-tambahP1.addEventListener("click", () => {
-  if (parseInt(skorP1.textContent) === winGame - 1) {
-    isWin = true;
-    skorP1.style.color = "green";
-    tambahP1.disabled = true;
-  }
-  skorP1.textContent = parseInt(skorP1.textContent) + 1;
-});
-
-tambahP2.addEventListener("click", () => {
-  if (parseInt(skorP2.textContent) === winGame - 1) {
-    isWin = true;
-    skorP2.style.color = "green";
-    tambahP2.disabled = true;
-  }
-  skorP2.textContent = parseInt(skorP2.textContent) + 1;
-});
-
-reset.addEventListener("click", () => {
+function resetScore() {
   (skorP1.textContent = 0), (skorP1.style.color = "black");
   (skorP2.textContent = 0), (skorP2.style.color = "black");
   tambahP1.disabled = false;
   tambahP2.disabled = false;
+  isWin = false;
+}
+
+winPoint.addEventListener("change", () => {
+  winGame = parseInt(winPoint.value);
+  resetScore();
 });
+
+tambahP1.addEventListener("click", () => {
+  if (!isWin) {
+    skorP1.textContent = parseInt(skorP1.textContent) + 1;
+    if (parseInt(skorP1.textContent) === winGame) {
+      isWin = true;
+      skorP1.style.color = "green";
+      tambahP1.disabled = true;
+    }
+  }
+});
+
+tambahP2.addEventListener("click", () => {
+  if (!isWin) {
+    skorP2.textContent = parseInt(skorP2.textContent) + 1;
+    if (parseInt(skorP2.textContent) === winGame) {
+      isWin = true;
+      skorP2.style.color = "green";
+      tambahP2.disabled = true;
+    }
+  }
+});
+
+reset.addEventListener("click", resetScore);
